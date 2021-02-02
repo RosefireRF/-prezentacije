54ac21d8287dedaa59f75c0659c4b92756cac31e
Appleiris <57938200+Appleiris@users.noreply.github.com>
2021-02-02 19:50:19 +0100

    Javascript Initialization
    Presentations now get added by a javascript function, iterating over an array of objects

---
+++ javascript.js
@@ -0,0 +1,96 @@
+let Objekti = [
+    {
+        ime : 'Ilija Krstić',
+        link : "https://docs.google.com/presentation/d/1nUV07CC8rhVj4JF4zcop2zpoT0lhs5ku/edit#slide=id.p1"
+    },
+    {
+        ime: 'Aleksandar Zečević',
+        link: "https://docs.google.com/presentation/d/1mYa4dn4z_pNaMwPFSCICsfIYy1Ar2eCr/edit?usp=drive_web&ouid=100857901141926325811&rtpof=true"
+    },
+    {
+        ime: "Irena Đukić",
+        link: "https://docs.google.com/presentation/d/1-KHgQx7xxe2kr9d4JOxS1rdMSAopxzdt/edit#slide=id.p1"
+    },
+    {
+        ime: "Lazar Bajić",
+        link: "https://docs.google.com/presentation/d/142MNNyE36x1l_d77rI7tMbvyIMR4D0WO/edit#slide=id.p1"
+    },
+    {
+        ime: "Dunja Vučinić",
+        link: "https://docs.google.com/presentation/d/11xoHz53HdEcryvcIuDss5gxQ7e2vkCXQ/edit#slide=id.p1"
+    },
+    {
+        ime: "Filip Jočić",
+        link: "https://drive.google.com/file/d/1A1rRNgTkKVyAz6WBl13tFyW-TY5aZrJ_/view?usp=sharing"
+    },
+    {
+        ime: "Iva Jovanović",
+        link: "https://docs.google.com/presentation/d/1S19V9sYgj1CT3L-S4QMXRByRIJoxaSuE/edit#slide=id.p1"
+    },
+    {
+        ime: "Ivan Rončević",
+        link: "https://docs.google.com/presentation/d/1crFF47Iqj41x-Kv6Tdm_Zd5M3rrcPIDc/edit?usp=drive_web&ouid=100857901141926325811&rtpof=true"
+    },
+    {
+        ime: "Katarina Marinović",
+        link: "https://docs.google.com/presentation/d/1AHPqhnjW5rE-A9ylGfVLVDgFdX6nJmXd/edit?usp=drive_web&ouid=100857901141926325811&rtpof=true"
+    },
+    {
+        ime: "Katarina Tadić",
+        link: "https://docs.google.com/presentation/d/18BkauOgNidzgAcUwyL0ty2Cxtm1sSPJe/edit?usp=drive_web&ouid=100857901141926325811&rtpof=true"
+    },
+    {
+        ime: "Manuela Gaši",
+        link: "https://docs.google.com/presentation/d/1Ujk7uEiOyROQdtXpiOkE-YvbPDUFkJUa/edit?usp=drive_web&ouid=100857901141926325811&rtpof=true"
+    },
+    {
+        ime: "Anastasija Stanković",
+        link: "https://docs.google.com/presentation/d/1xfJev3HSqCFnSjfHoL0W7CHWLfYRSxkR/edit?usp=drive_web&ouid=100857901141926325811&rtpof=true"
+    },
+    {
+        ime: "Ana Stanković",
+        link: "https://docs.google.com/presentation/d/1XRnfZbYh4C5c-z4bIzLtaz30rsgNUf4P/edit#slide=id.p1"
+    },
+    {
+        ime: "Tijana Mrkaljević",
+        link: "https://docs.google.com/presentation/d/1j-SiAPPeEbDRuNaJBKICQvJkIws5JRax/edit?usp=drive_web&ouid=100857901141926325811&rtpof=true"
+    },
+    {
+        ime: "Tijana Jusup",
+        link: "https://docs.google.com/presentation/d/153Q4pS0sL4uKFMwgrEisOrfCHAhV6WwM/edit?usp=drive_web&ouid=100857901141926325811&rtpof=true"
+    },
+    {
+        ime: "Srđan Ćulum",
+        link: "https://docs.google.com/presentation/d/1jY9cc3u3lttY5jk7xMjKJIf3JfqgVc-G/edit?usp=drive_web&ouid=100857901141926325811&rtpof=true"
+    },
+    {
+        ime: "Vesna Filipović",
+        link: "https://drive.google.com/file/d/1oSkQ81SXTiAUObHuQNtlSCmzQ0b5IEuy/view?usp=sharing"
+    },
+    {
+        ime: "Tifani Đorđević",
+        link: "https://docs.google.com/presentation/d/1qWah4OoS0EoVGNBkfrpkHJ62FfTOiHou/edit#slide=id.p1"
+    },
+    {
+        ime: "Jelena Sajovic",
+        link: "https://docs.google.com/presentation/d/16LOv7PNLnv-6N9LWM7gAjjcKK9MPN6ML/edit#slide=id.p1"
+    },
+    {
+        ime: "Ognjen Jovanović",
+        link: "https://docs.google.com/presentation/d/1LSRwbESgolJOAHHxMCCM-aX1u3VMmANN/edit#slide=id.p1"
+    },
+    {
+        ime: "Nikola Škoja",
+        link: "https://docs.google.com/presentation/d/15Asw4q7X-otEnbqDAtFyBDeD3SN_zim6/edit#slide=id.p1"
+    }
+]
+
+function stvaranje(){
+let stvariZaPrikazati = "";
+    for (let prezentacija of Objekti){
+        let link = "'" + prezentacija.link + "'";
+        stvariZaPrikazati += "<a href=" + link + "target='_blank'><div class='prezentacija'>" + prezentacija.ime +"</div></a>";
+    }
+    document.getElementById("prikaz").innerHTML += stvariZaPrikazati;
+}
+stvaranje();
